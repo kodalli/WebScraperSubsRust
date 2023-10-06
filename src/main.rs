0d932f98c859facbd784403b7dc24c3e90d97acc
kodalli <surya.kodali@gmail.com>
2023-10-05 23:30:43 -0400

    using transmission rpc instead of webdriver, refactored code

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,49 @@
-use std::{
-    panic,
-    process::{Child, Command},
-};
+use std::collections::VecDeque;
+use std::process::{Child, Command};
 
+use anyhow::{anyhow, Ok, Result};
 use thirtyfour::prelude::*;
 
 #[tokio::main]
-async fn main() -> WebDriverResult<()> {
-
+async fn main() -> Result<()> {
     // Prompt user input
-    let mut sp_title = String::new();
-    println!("Enter the subsplease title: ");
-    std::io::stdin().read_line(&mut sp_title).expect("Could not read arg");
-    sp_title = sp_title.replace("–", "-").trim().to_string();
-
-    let mut season_str = String::new();
-    println!("Enter the season: ");
-    std::io::stdin().read_line(&mut season_str).expect("Could not read arg");
-    let season_number = season_str.trim().parse::<u8>().unwrap();
-
-    let mut batch_str = String::new();
-    println!("Enter true or false for batch download: ");
-    std::io::stdin().read_line(&mut batch_str).expect("Could not read arg");
-    let batch = batch_str.trim().parse::<bool>().unwrap();
-
+    let (sp_title, season_number, batch) = get_user_input();
     // This needs a running web driver like chromedriver or geckodriver
-
-    let magnet_links = get_magnet_links_from_subsplease(&sp_title, batch).await;
-
-    match magnet_links {
-        Ok(links) => {
-            for link in links {
-                let result = panic::catch_unwind(|| async {
-                    upload_to_transmission(&link, &sp_title, season_number).await
-                });
-
-                match result {
-                    Ok(future_result) => {
-                        let web_driver_result = future_result.await;
-                        if let Err(e) = web_driver_result {
-                            eprintln!("Error in WebDriver logic: {:?}", e);
-                        }
-                    }
-                    Err(panic_info) => {
-                        eprintln!("Program panicked: {:?}", panic_info);
-                    }
-                }
-            }
-        },
-        Err(err) => eprintln!("Failed to get magnet links {:?}", err),
-    }
+    let magnet_links = get_magnet_links_from_subsplease(&sp_title, batch).await?;
+    let _result = upload_to_transmission_rpc(magnet_links, &sp_title, season_number).await?;
 
     Ok(())
 }
 
-async fn get_magnet_links_from_subsplease(sp_title: &str, batch_if_available: bool) -> WebDriverResult<Vec<String>> {
+fn get_user_input() -> (String, u8, bool) {
+    let mut sp_title = String::new();
+    println!("Enter the subsplease title: ");
+    std::io::stdin()
+        .read_line(&mut sp_title)
+        .expect("Could not read arg");
+    sp_title = sp_title.replace("–", "-").trim().to_string();
+
+    let mut season_str = String::new();
+    println!("Enter the season: ");
+    std::io::stdin()
+        .read_line(&mut season_str)
+        .expect("Could not read arg");
+    let season_number = season_str.trim().parse::<u8>().unwrap();
+
+    let mut batch_str = String::new();
+    println!("Enter true or false for batch download: ");
+    std::io::stdin()
+        .read_line(&mut batch_str)
+        .expect("Could not read arg");
+    let batch = batch_str.trim().parse::<bool>().unwrap();
+
+    (sp_title, season_number, batch)
+}
+
+async fn get_magnet_links_from_subsplease(
+    sp_title: &str,
+    batch_if_available: bool,
+) -> Result<Vec<String>> {
     let driver_process = DriverProcess::new("geckodriver", 4444);
     let port = driver_process.port();
 
@@ -62,7 +51,8 @@ async fn get_magnet_links_from_subsplease(sp_title: &str, batch_if_available: bo
 
     // WebDriver
     let mut caps = DesiredCapabilities::firefox();
-    caps.set_headless().expect("Failed to set browser to headless mode");
+    caps.set_headless()
+        .expect("Failed to set browser to headless mode");
     let driver = WebDriver::new(&format!("http://localhost:{}", port), caps)
         .await
         .expect("Failed to create WebDriver");
@@ -77,8 +67,15 @@ async fn get_magnet_links_from_subsplease(sp_title: &str, batch_if_available: bo
     println!("Reached SubsPlease");
 
     let anime_xp = &format!("//a[text()=\"{}\"]", sp_title);
-    let anime_elem = driver.find(By::XPath(anime_xp)).await.expect("Failed to find anime link");
-    let anime_link = anime_elem.attr("href").await.expect("Failed to find href attribute").expect("Failed to find anime link");
+    let anime_elem = driver
+        .find(By::XPath(anime_xp))
+        .await
+        .expect("Failed to find anime link");
+    let anime_link = anime_elem
+        .attr("href")
+        .await
+        .expect("Failed to find href attribute")
+        .expect("Failed to find anime link");
 
     driver
         .goto(anime_link)
@@ -91,81 +88,85 @@ async fn get_magnet_links_from_subsplease(sp_title: &str, batch_if_available: bo
     let batch_elem = driver.find(By::XPath(batch_xp)).await;
 
     let magnet_xp = "//a[contains(@href,'1080p')]/span[text()='Magnet']/..";
-    let magnet_elems = driver.find_all(By::XPath(magnet_xp)).await;
+    let magnet_elems = driver
+        .find_all(By::XPath(magnet_xp))
+        .await
+        .expect("Failed to get magnet links");
 
-    let mut magnet_links: Vec<String> = Vec::new();
-    match magnet_elems {
-       Ok(mag_elems) => {
-            if batch_if_available && batch_elem.is_ok() {
-                let mag_link = mag_elems.get(0).unwrap().attr("href").await;
-                if mag_link.is_ok() {
-                    magnet_links.push(mag_link.unwrap().unwrap());
-                }
-            } else {
-                for mag_elem in mag_elems {
-                    let mag_link = mag_elem.attr("href").await;
-                    if mag_link.is_ok() {
-                        magnet_links.push(mag_link.unwrap().unwrap());
-                    }
-                }
-            }
-            println!("Retrieved Magent Links");
-       },
-       Err(err) => {
-           eprintln!("Could not find magnet links: {:?}", err);
-       }
+    let mut magnet_links: VecDeque<String> = VecDeque::new();
+
+    for mag_ele in magnet_elems {
+        let result = mag_ele.attr("href").await?;
+        if result.is_some() {
+            magnet_links.push_back(result.unwrap());
+        }
     }
-    return Ok(magnet_links)
-}
 
-async fn upload_to_transmission(link: &str, show_name: &str, season_number: u8) -> WebDriverResult<()> {
-    let driver_process = DriverProcess::new("geckodriver", 4444);
-    let port = driver_process.port();
+    println!("Retrieved magnet links: {:?}", magnet_links.len());
 
-    println!("Starting WebDriver");
+    if batch_elem.is_ok() {
+        let batch_link = magnet_links.pop_front();
+        if batch_if_available && batch_link.is_some() {
+            magnet_links.clear();
+            magnet_links.push_back(batch_link.unwrap());
+        }
+    }
 
-    // WebDriver
-    let mut caps = DesiredCapabilities::firefox();
-    caps.set_headless().expect("Failed to set browser to headless mode");
-    let driver = WebDriver::new(&format!("http://localhost:{}", port), caps)
-        .await
-        .expect("Failed to create WebDriver");
-
-    let transmission_url = "http://192.168.86.71:9091/transmission/web/";
-    driver
-        .goto(transmission_url)
-        .await
-        .expect("Failed to navigate to URL");
-
-    println!("Reached Transmission!");
-
-    let open_xp = "//*[@id=\"toolbar-open\"]";
-    let torrent_upload_url_xp = "//*[@id=\"torrent_upload_url\"]";
-    let save_location_xp = "//*[@id=\"add-dialog-folder-input\"]";
-    let upload_xp = "//*[@id=\"upload_confirm_button\"]";
-
-    let upload_elem = driver.find(By::XPath(open_xp)).await.expect("Failed to find open button");
-    upload_elem.click().await.expect("Failed to click open button");
-
-    println!("Clicked Open Button!");
-
-    let url_elem = driver.find(By::XPath(torrent_upload_url_xp)).await.expect("Failed to find url element");
-    url_elem.clear().await.expect("Failed to clear url element");
-    url_elem.send_keys(link).await.expect("Failed to update url element");
-
-    let destination_elem = driver.find(By::XPath(save_location_xp)).await.expect("Failed to find folder element");
-    destination_elem.clear().await.expect("Failed to clear folder element");
-    destination_elem.send_keys(&format!("/data/Anime/{}/Season {}/", show_name, season_number)).await.expect("Failed to update folder element");
-
-    let upload_button = driver.find(By::XPath(upload_xp)).await.expect("Failed to find upload button");
-    upload_button.click().await.expect("Failed to click upload button");
-
-    println!("Queued torrent for {}", show_name);
-
-    // Close Browser
     driver.quit().await.expect("Failed to quit WebDriver");
 
-    println!("Closed WebDriver");
+    println!("Final magnet links: {:?}", magnet_links.len());
+
+    return Ok(magnet_links.into());
+}
+
+async fn upload_to_transmission_rpc(
+    links: Vec<String>,
+    show_name: &str,
+    season_number: u8,
+) -> Result<()> {
+    let url = "http://192.168.86.71:9091/transmission/rpc";
+
+    let client = reqwest::Client::new();
+    let resp = client.get(url).send().await?;
+    let session_id = resp
+        .headers()
+        .get("X-Transmission-Session-Id")
+        .ok_or_else(|| anyhow!("Missing X-Transmission-Session-Id header"))?
+        .to_str()
+        .map_err(|_| anyhow!("Invalid X-Transmission-Session-Id header"))?;
+
+    println!("Recieved session_id from transmission");
+
+    let destination_folder = format!("/data/Anime/{}/Season {}/", show_name, season_number);
+
+    let count = links.len();
+    for magnet_link in links {
+        let body = format!(
+            r#"{{
+            "method": "torrent-add",
+            "arguments": {{
+                "filename": "{}",
+                "download-dir": "{}"
+            }}"#,
+            magnet_link, destination_folder
+        );
+
+        let post_resp = client
+            .post(url)
+            .header("X-Transmission-Session-Id", session_id)
+            .header("Content-Type", "application/json")
+            .body(body)
+            .send()
+            .await?;
+
+        println!(
+            "Response for {}: {:?}",
+            magnet_link,
+            post_resp.text().await?
+        );
+    }
+
+    println!("Sent links to transmission: {:?}", count);
 
     Ok(())
 }
@@ -179,7 +180,7 @@ struct DriverProcess {
 
 impl DriverProcess {
     fn new(command: &str, desired_port: u16) -> Self {
-        // geckodriver chooses a random port :(
+        // Specify port for geckodriver
         let child = Command::new(command)
             .arg("-p")
             .arg(desired_port.to_string())
@@ -203,5 +204,34 @@ impl Drop for DriverProcess {
             let _ = child.kill(); // Kill child process
             let _ = child.wait(); // Wait for the process to terminate
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_scrape_subs_and_upload_batch_true() {
+        let sp_title = "Sousou no Frieren".to_string();
+        let season_number = 1;
+        let batch = true;
+        let magnet_links = get_magnet_links_from_subsplease(&sp_title, batch).await;
+        assert!(magnet_links.is_ok());
+        let result =
+            upload_to_transmission_rpc(magnet_links.unwrap(), &sp_title, season_number).await;
+        assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_scrape_subs_and_upload_batch_false() {
+        let sp_title = "Sousou no Frieren".to_string();
+        let season_number = 1;
+        let batch = false;
+        let magnet_links = get_magnet_links_from_subsplease(&sp_title, batch).await;
+        assert!(magnet_links.is_ok());
+        let result =
+            upload_to_transmission_rpc(magnet_links.unwrap(), &sp_title, season_number).await;
+        assert!(result.is_ok());
     }
 }
