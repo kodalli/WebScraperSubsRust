9a3d59827b67c19d8b2ec8840c091339f01c1609
kodalli <surya.kodali@gmail.com>
2023-10-05 23:38:55 -0400

    fixed tests, added port field

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ async fn main() -> Result<()> {
     // Prompt user input
     let (sp_title, season_number, batch) = get_user_input();
     // This needs a running web driver like chromedriver or geckodriver
-    let magnet_links = get_magnet_links_from_subsplease(&sp_title, batch).await?;
+    let magnet_links = get_magnet_links_from_subsplease(&sp_title, batch, 4444).await?;
     let _result = upload_to_transmission_rpc(magnet_links, &sp_title, season_number).await?;
 
     Ok(())
@@ -43,8 +43,9 @@ fn get_user_input() -> (String, u8, bool) {
 async fn get_magnet_links_from_subsplease(
     sp_title: &str,
     batch_if_available: bool,
+    geckoriver_port: u16,
 ) -> Result<Vec<String>> {
-    let driver_process = DriverProcess::new("geckodriver", 4444);
+    let driver_process = DriverProcess::new("geckodriver", geckoriver_port);
     let port = driver_process.port();
 
     println!("Starting WebDriver");
@@ -216,7 +217,7 @@ mod test {
         let sp_title = "Sousou no Frieren".to_string();
         let season_number = 1;
         let batch = true;
-        let magnet_links = get_magnet_links_from_subsplease(&sp_title, batch).await;
+        let magnet_links = get_magnet_links_from_subsplease(&sp_title, batch, 4444).await;
         assert!(magnet_links.is_ok());
         let result =
             upload_to_transmission_rpc(magnet_links.unwrap(), &sp_title, season_number).await;
@@ -228,7 +229,7 @@ mod test {
         let sp_title = "Sousou no Frieren".to_string();
         let season_number = 1;
         let batch = false;
-        let magnet_links = get_magnet_links_from_subsplease(&sp_title, batch).await;
+        let magnet_links = get_magnet_links_from_subsplease(&sp_title, batch, 4445).await;
         assert!(magnet_links.is_ok());
         let result =
             upload_to_transmission_rpc(magnet_links.unwrap(), &sp_title, season_number).await;
